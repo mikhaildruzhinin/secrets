d48901911fb33b7f77db82af0d28776aebfb8c0e
Mikhail Druzhinin <druzhininmy@gmail.com>
2020-05-13 18:10:20 +0300

    fixed an error with the file storing secrets

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi import HTTPException
 from pydantic import BaseModel
+from pathlib import Path
 import secrets
 import string
 import pickle
@@ -16,30 +17,41 @@ secret_db = {}
 
 alphabet = string.ascii_letters + string.digits
 
-secret_app = FastAPI()
+app = FastAPI()
 
-@secret_app.post('/generate/')
+@app.post('/generate/')
 def generate_secret(secret: Secret):
     # secret_key = ''.join(secrets.choice(alphabet) for i in range(20))
     secret_key = 'aaa'
     secret_db[secret_key] = {'text': secret.text, 'code_phrase': secret.code_phrase }
+
+    Path('files').mkdir(parents=True, exist_ok=True)
+    Path('files/data.txt').touch(exist_ok=True)
     with open('files/data.txt', 'wb') as src:
         pickle.dump(secret_db, src, protocol=3)
     return {'secret_key': secret_key}
 
-@secret_app.post('/secrets/{secret_key}/')
+@app.post('/secrets/{secret_key}/')
 def get_secret(secret_key: str, code: Code):
+
+    Path('files').mkdir(parents=True, exist_ok=True)
+    Path('files/data.txt').touch(exist_ok=True)
+
     with open('files/data.txt', 'rb') as src:
         secret_db = pickle.load(src)
     if secret_key not in secret_db.keys():
         raise HTTPException(status_code=404, detail='key not found')
     if code.code_phrase == secret_db[secret_key]['code_phrase']:
         secret = secret_db.pop(secret_key)
+
+        Path('files').mkdir(parents=True, exist_ok=True)
+        Path('files/data.txt').touch(exist_ok=True)
+
         with open('files/data.txt', 'wb') as src:
             pickle.dump(secret_db, src, protocol=3)
         return {'secret': secret['text']}
     raise HTTPException(status_code=404, detail='invalid code')
 
-@secret_app.get('/')
+@app.get('/')
 def get_root():
-    return secret_db
+    return {'hello': 'world'}
