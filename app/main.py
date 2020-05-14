7085aa8f4243bf70bd304a7105e0e7981b4be4fc
Mikhail Druzhinin <druzhininmy@gmail.com>
2020-05-14 12:47:11 +0300

    update main.py to meet pep8 requirements

--- app/main.py
+++ app/main.py
@@ -6,9 +6,11 @@ import secrets
 import string
 import pickle
 
+
 class Secret(BaseModel):
     text: str
     code_phrase: str
+
 
 class Code(BaseModel):
     code_phrase: str
@@ -19,17 +21,22 @@ alphabet = string.ascii_letters + string.digits
 
 app = FastAPI()
 
+
 @app.post('/generate/')
 def generate_secret(secret: Secret):
     # secret_key = ''.join(secrets.choice(alphabet) for i in range(20))
     secret_key = 'aaa'
-    secret_db[secret_key] = {'text': secret.text, 'code_phrase': secret.code_phrase }
+    secret_db[secret_key] = {
+        'text': secret.text,
+        'code_phrase': secret.code_phrase
+    }
 
     Path('files').mkdir(parents=True, exist_ok=True)
     Path('files/data.txt').touch(exist_ok=True)
     with open('files/data.txt', 'wb') as src:
         pickle.dump(secret_db, src, protocol=3)
     return {'secret_key': secret_key}
+
 
 @app.post('/secrets/{secret_key}/')
 def get_secret(secret_key: str, code: Code):
@@ -51,6 +58,7 @@ def get_secret(secret_key: str, code: Code):
             pickle.dump(secret_db, src, protocol=3)
         return {'secret': secret['text']}
     raise HTTPException(status_code=404, detail='invalid code')
+
 
 @app.get('/')
 def get_root():
